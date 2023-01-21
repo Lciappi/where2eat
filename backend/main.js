05a769da423e2116390ef338e23ca639fe27dacb
sebastiangonzalez <bastigonzalez2000@gmail.com>
2023-01-21 12:36:33 -0800

    Getting query

--- backend/main.js
+++ backend/main.js
@@ -2,16 +2,26 @@ var map;
 var service;
 var infowindow;
 
-function initialize() {
-  var vancouver = new google.maps.LatLng(49.316666, -123.066666);
+function getQuery() {
+  let prompt = document.currentScript.getAttribute("prompt");
+  let address = document.currentScript.getAttribute("address");
+  let time = document.currentScript.getAttribute("time");
+
+  console.log("Prompt: " + prompt);
+  console.log("Address: " + prompt);
+  console.log("Time: " + prompt);
+
+  let coords = getCoords(address);
+
+  var location = new google.maps.LatLng(coords.lat, coords.lng);
 
   map = new google.maps.Map(document.getElementById("map"), {
-    center: vancouver,
+    center: location,
     zoom: 15,
   });
 
   var request = {
-    location: vancouver,
+    location: location,
     radius: "500",
     query: "I want to eat very cheap sushi",
   };
@@ -24,7 +34,7 @@ function callback(results, status) {
   if (status == google.maps.places.PlacesServiceStatus.OK) {
     for (var i = 0; i < results.length; i++) {
       var place = results[i];
-      console.log(place.name);
+      console.log(place);
     }
   }
 }
