0de3cf9eac0f4d9cd166c13e83626e1c4add3325
nicoserr <101030492+nicoserr@users.noreply.github.com>
2023-01-21 13:34:07 -0800

    many changes

--- backend/main.js
+++ backend/main.js
@@ -1,8 +1,11 @@
 var map;
+var geocoder;
 var service;
 var infowindow;
 
-function getQuery() {
+async function getQuery() {
+  geocoder = new google.maps.Geocoder();
+
   let prompt = document.currentScript.getAttribute("prompt");
   let address = document.currentScript.getAttribute("address");
   let time = document.currentScript.getAttribute("time");
@@ -11,14 +14,17 @@ function getQuery() {
   console.log("Address: " + prompt);
   console.log("Time: " + prompt);
 
-  let coords = getCoords(address);
+  let coords = await getCoords(address);
+  console.log(coords);
 
-  var location = new google.maps.LatLng(coords.lat, coords.lng);
-
+  var location = new google.maps.LatLng(49.2887722, -123.1217156);
+  
   map = new google.maps.Map(document.getElementById("map"), {
     center: location,
     zoom: 15,
   });
+  
+  service = new google.maps.places.PlacesService(map);
 
   var request = {
     location: location,
@@ -26,7 +32,6 @@ function getQuery() {
     query: "I want to eat very cheap sushi",
   };
 
-  service = new google.maps.places.PlacesService(map);
   service.textSearch(request, callback);
 }
 
@@ -34,20 +39,25 @@ function callback(results, status) {
   if (status == google.maps.places.PlacesServiceStatus.OK) {
     for (var i = 0; i < results.length; i++) {
       var place = results[i];
-      console.log(place);
+      // console.log(place);
     }
   }
 }
 
-function getCoords(address) {
-    const request = {
-        query: address,
-        fields: ['geometry'],
+async function getCoords(address) {
+  geocoder.geocode({'address': address}, function(results, status) {
+    if (status === google.maps.places.PlacesServiceStatus.OK) {
+      console.log(results);
+      var lat = results[0].geometry.location.lat();
+      var lng = results[0].geometry.location.lng();
+      var returnBody = {
+        'lat': lat,
+        'lng': lng
       };
-
-    service.textSearch(request, function(results, status) {
-        if (status === google.maps.places.PlacesServiceStatus.OK) {
-          return results[0].geometry.location;
-        }
-      });
+      console.log(returnBody);
+      return returnBody;
+    } else {
+      alert('Geocode was not successful for the following reason: ' + status);
+    }
+  });
 }
