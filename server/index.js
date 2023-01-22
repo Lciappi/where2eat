41a95aa982bea178cc4057385485c7a1bc9f9892
sebastiangonzalez <bastigonzalez2000@gmail.com>
2023-01-21 17:29:13 -0800

    Server

--- server/index.js
+++ server/index.js
@@ -1,11 +1,22 @@
 const express = require("express");
+const axios = require("axios");
 
 const PORT = 3000;
+const API_KEY = "AIzaSyCxfqw7KcnonT2CCLi6Y7CfJpr2GULAJ_M";
 
 const app = express();
 
-app.get("/recomend", (req, res) => {
-  res.json({ first: "first place" });
+app.get("/recommend", (req, res) => {
+  const BASE_URL = `https://maps.googleapis.com/maps/api/place/textsearch/json
+    ?location=42.3675294%2C-71.186966
+    &query=123%20main%20street
+    &radius=10000
+    &key=${API_KEY}`;
+
+  return axios.get(BASE_URL).then((response) => {
+    console.log(repsonse);
+    res.json({ first: "first place" });
+  });
 });
 
 app.listen(PORT, () => console.log(`Server is now running on port ${PORT}`));
