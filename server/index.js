a867c93e108ce6b1b40377e88a41ceaf24a79acd
nicoserr <101030492+nicoserr@users.noreply.github.com>
2023-01-21 19:25:35 -0800

    added no address found handling

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ app.get("/recommend", (req, res) => {
   const radius = req.radius;
 
   getCoords(address).then((location) => {
-    if(location == null) {
+    if (location == null) {
       res.status(500);
       return res.json({error: "No address found"});
     }
@@ -29,7 +29,6 @@ app.get("/recommend", (req, res) => {
 
     axios(config)
       .then(function (response) {
-        console.log(response.data);
         res.json({ first: "first place" });
       })
       .catch(function (error) {
@@ -38,18 +37,20 @@ app.get("/recommend", (req, res) => {
   });
 });
 
-app.listen(PORT, () => console.log(`Server is now running on port ${PORT}`));
-
 function getCoords(address) {
   const encodedAddress = encodeURIComponent(address);
   const requestUrl =
     GEOCODE_BASE_URL + "?address=" + encodedAddress + `&key=${API_KEY}`;
 
   return axios.get(requestUrl).then((response) => {
-    if (response.status != 'OK') {
+    if (response.status != 200) {
       return null;
     } else {
       return response.data.results[0].geometry.location;
     }
   });
 }
+
+
+
+app.listen(PORT, () => console.log(`Server is now running on port ${PORT}`));
