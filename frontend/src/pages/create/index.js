a11e4e05f612d6ca546eb49871fa4fc9e8d78917
nicoserr <101030492+nicoserr@users.noreply.github.com>
2023-01-23 14:25:09 -0800

    moved api key into local environment and added file path to gitignore

--- frontend/src/pages/create/index.js
+++ frontend/src/pages/create/index.js
@@ -15,13 +15,11 @@ import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
 import parse from "autosuggest-highlight/parse";
 import { debounce } from "@mui/material/utils";
-import dotenv from 'dotenv';
-dotenv.config();
 
 
 // This key was created specifically for the demo in mui.com.
 // You need to create a new one for your application.
-const GOOGLE_MAPS_API_KEY = process.env.API_KEY;
+const GOOGLE_MAPS_API_KEY = process.env.NEXT_PUBLIC_API_KEY;
 
 function loadScript(src, position, id) {
   if (!position) {
